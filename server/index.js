9097e8cb72f0f55197fd149a0b8dffedce4aa8ee
Gagik Simonyan <simonyan.gagik@softconstruct.com>
2021-10-22 18:34:41 +0400

    Server version

--- server/index.js
+++ server/index.js
@@ -1,27 +1,59 @@
-const express = require('express');
-let app = express();
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+});
 
-const http = require('http');
-let server = http.Server(app);
-
-const socketIO = require('socket.io');
-let io = socketIO(server);
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log(`Started on port: ${port}`);
+app.get('/', (req, res) => {
+    res.send('<h1>Hey Socket.io</h1>');
 });
 
 io.on('connection', (socket) => {
-    socket.on('join', (data) => {
-        socket.join(data.room);
-        socket.broadcast.to(data.room).emit('user joined');
-    })
+    console.log('a user connected');
 
-    socket.on('message', (data) => {
-        io.in(data.room).emit('new message', {user: data.user, message: data.message});
-        
-    })
-})
+    socket.on('my message', (msg) => {
+        // console.log('message: ' + msg);
+        io.emit('my broadcast', `server: ${msg}`);
+    });
+});
+
+http.listen(3001, () => {
+    console.log('listening on port :3001');
+});
+
+
+
+
+
+
+
+
+
+// let express = require('express');
+// let app = express();
+//
+// let http = require('http');
+// let server = http.Server(app);
+//
+// let socketIO = require('socket.io');
+// let io = socketIO(server);
+//
+// const port = process.env.PORT || 3001;
+//
+// io.on('connection', (socket) => {
+//     socket.on('join', (data) => {
+//         socket.join(data.room);
+//         socket.broadcast.to(data.room).emit('user joined');
+//     });
+//
+//     socket.on('message', (data) => {
+//         io.in(data.room).emit('new message', {user: data.user, message: data.message});
+//     });
+// })
+//
+// server.listen(port, () => {
+//     console.log(`started on port: ${port}`);
+// });
 
